dd7d29601801a0844a21f3666097754a7be5507d
Nik Charlebois <nik_charlebois@hotmail.com>
2025-04-30 12:31:43 -0400

    Fixes

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 const msalConfig = {
   auth: {
-    clientId: 'c6336297-ebbf-4cb2-bf22-5304363a3618',//'006ad752-04b5-4292-9a9e-8d08668edf31',
+    clientId: '006ad752-04b5-4292-9a9e-8d08668edf31',
     redirectUri: 'http://localhost:8080'
   }
 };
