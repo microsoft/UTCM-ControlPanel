002da9ac1e4ccac7feff100de6271157919ad462
Nik Charlebois <nik_charlebois@hotmail.com>
2025-05-08 09:16:01 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -971,12 +971,12 @@ function showMonitors(monitors, runs, graphURI) {
             driftcell = createElement('td', 'subValueRed', null);
             let driftLink = createElement('a', null, run.driftsCount + " Drift(s) Detected <img src='red.jpg' width='20' alt='" + run.driftsCount + " drifts detected' />")
             driftLink.setAttribute('onclick', 'getDrifts("' + monitor.id + '");');
-            driftcell.appendChild(driftLink);
+            driftcell.innerHtml = "<img src='red.jpg' width='20' alt='" + run.driftsCount + " drifts detected' />&nbsp;<a onclick='getDrifts(\"" + monitor.id+ "\")'>" + run.driftsCount + " Drift(s) Detected</a>";
           }
           else
           {
             driftcell = createElement('td');
-            driftcell.innerHTML = run.driftsCount + " Drift(s) Detected <img src='green.png' width='20' alt='No drift detected' />"
+            driftcell.innerHTML = run.driftsCount + "<img src='green.png' width='20' alt='No drift detected' />&nbsp;Drift(s) Detected"
           }
           runrow.appendChild(driftcell);
 
