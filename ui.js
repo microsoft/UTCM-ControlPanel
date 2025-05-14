2873f159accfd5b155c1b9a5d099150a1a86087f
Nik Charlebois <nik_charlebois@hotmail.com>
2025-05-14 12:38:57 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -589,11 +589,36 @@ function showReport()
   mainContainer.appendChild(report);
 }
 
+function sortByProperty(objArray, prop, direction){
+    if (arguments.length<2) throw new Error("ARRAY, AND OBJECT PROPERTY MINIMUM ARGUMENTS, OPTIONAL DIRECTION");
+    if (!Array.isArray(objArray)) throw new Error("FIRST ARGUMENT NOT AN ARRAY");
+    const clone = objArray.slice(0);
+    const direct = arguments.length>2 ? arguments[2] : 1; //Default to ascending
+    const propPath = (prop.constructor===Array) ? prop : prop.split(".");
+    clone.sort(function(a,b){
+        for (let p in propPath){
+                if (a[propPath[p]] && b[propPath[p]]){
+                    a = a[propPath[p]];
+                    b = b[propPath[p]];
+                }
+        }
+        // convert numeric strings to integers
+        a = a.match(/^\d+$/) ? +a : a;
+        b = b.match(/^\d+$/) ? +b : b;
+        return ( (a < b) ? -1*direct : ((a > b) ? 1*direct : 0) );
+    });
+    return clone;
+}
+
 function showSnapshot(data, graphURI) {
 
   delete data.id;
   delete data['@odata.context'];
+
+  var sortedResources = sortByProperty(data.resources, 'resourceType');
+  data.resources = sortedResources;
   var snapshotContent = JSON.stringify(data, null, 4);
+
   let divCountResources = document.createElement('div')
   divCountResources.innerHTML = "<strong>This snapshot contains:</strong> " + data.resources.length + " resources&nbsp;<a onclick='showReport();'><img src='images/report.png' alt='Generate report' width='25px' /></a>";
   let form = document.createElement('form');
